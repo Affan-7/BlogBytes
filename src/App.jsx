291e18a42989e97d5b0a5d183bfe38bc938edce5
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-02-04 13:34:37 +0530

    Add prettier extenstion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "./components/Home/Home"
-import Header from "./components/Header/Header"
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
+      <Header />
+      <Home />
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 const Header = () => {
   return (
     <header>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+      <script
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
+        integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL'
+        crossorigin='anonymous'
+      ></script>
+      <nav class='navbar bg-body-tertiary'>
+        <div class='container-fluid'>
+          <a class='navbar-brand' href='#'>
+            Navbar
+          </a>
+        </div>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
