56163ac2721d990058d35351f2a7f08e15a97f5c
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-02-04 18:29:28 +0530

    Add shadow to the navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,10 @@ const Header = () => {
         integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL'
         crossOrigin='anonymous'
       ></script>
-      <nav className='navbar d-flex justify-content-start mt-1'>
+      <nav
+        className='navbar d-flex justify-content-start'
+        style={{ boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)' }}
+      >
         <a className='navbar-brand ps-4' href='/'>
           <span
             className='bg-dark text-white px-2 py-2 rounded-1'
