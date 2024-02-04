033455ef04efec5c075c95296fd608ebda9f663c
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-02-04 17:49:44 +0530

    Add BlogBytes logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,17 @@ const Header = () => {
       <script
         src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
         integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL'
-        crossorigin='anonymous'
+        crossOrigin='anonymous'
       ></script>
-      <nav class='navbar bg-body-tertiary'>
-        <div class='container-fluid'>
-          <a class='navbar-brand' href='#'>
-            Navbar
-          </a>
-        </div>
+      <nav className='navbar d-flex justify-content-start mt-1'>
+        <a className='navbar-brand ps-4' href='/'>
+          <span
+            className='bg-dark text-white px-2 py-2 rounded-1'
+            style={{ fontFamily: 'Rubik' }}
+          >
+            BlogBytes
+          </span>
+        </a>
       </nav>
     </header>
   )
