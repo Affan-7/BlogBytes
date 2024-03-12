633036e85cce5d278c8059c89afb2f33bc23dd2c
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-03-12 17:31:47 +0530

    Add landing page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function LandingPage() {
   return (
     <>
-      <h1>Landing Page</h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '90vh',
+        }}
+      >
+        <p style={{ fontSize: '70px', textAlign: 'center' }}>
+          Discover, connect,
+          <br />
+          share
+        </p>
+        <Link to='/register' style={{ width: '15%' }} className='mt-5'>
+          <button
+            className='btn btn-outline-secondary hover'
+            style={{ width: '100%' }}
+          >
+            Get started
+          </button>
+        </Link>
+      </div>
     </>
   )
 }
